f9fb664559433c5310d96cd7ef3f1beb0adb20d7
Lorenzo Pirrone <lore.pirro02@gmail.com>
2026-08-05 20:24:30 +0200

    Incoming Obstacle FIX

--- spazio-terzo/scripts/incoming_obstacle.gd
+++ spazio-terzo/scripts/incoming_obstacle.gd
@@ -1,123 +1,103 @@
 extends Node2D
 
-## Velocità con cui l'ostacolo entra da destra verso sinistra.
-@export var speed: float = 560.0
-## Distanza dalla camera a cui il trigger attiva il warning.
-@export var trigger_distance: float = 760.0
-## Tempo di preavviso visivo prima che l'ostacolo arrivi.
-@export var warning_time: float = 0.75
-## Tempo stimato tra warning e impatto effettivo.
-@export var travel_time_to_impact: float = 0.95
-## Distanza laterale iniziale dello spawn rispetto alla camera.
-@export var spawn_offset_from_camera: float = 90.0
-## Margine oltre il quale l'ostacolo viene rimosso a sinistra.
-@export var despawn_left_margin: float = 260.0
-## Consente o meno di colpire l'ostacolo con il cazzotto.
-@export var can_be_punched: bool = true
-## Distanza dal bordo destro in cui compare il punto esclamativo rosso.
-@export var warning_right_margin: float = 56.0
-## Offset verticale del warning rispetto alla posizione dell'ostacolo.
-@export var warning_vertical_offset: float = -86.0
+@export var alert_duration: float = 0.85
+@export var obstacle_speed: float = 620.0
+@export var self_destruct_time: float = 4.0
+
+const WARNING_RIGHT_MARGIN := 56.0
+const SPAWN_OFFSCREEN_MARGIN := 96.0
 
 @onready var trigger_box: Area2D = $TriggerBox
-@onready var impact_point: Marker2D = $ImpactPoint
 @onready var warning: Label = $Warning
-@onready var missile: Area2D = $Missile
+@onready var obstacle: Area2D = $Obstacle
 
-var _trigger_x: float
-var _lane_y: float
-var _state := "waiting"
-var _warning_timer: float = 0.0
-var _travel_timer: float = 0.0
-var _launch_speed: float = 0.0
-var _player: Node2D
+var _lane_y: float = 0.0
+var _camera: Camera2D
+var _state: StringName = &"waiting"
+var _alert_timer: float = 0.0
+var _life_timer: float = 0.0
 
 
 func _ready() -> void:
-	_trigger_x = global_position.x
 	_lane_y = global_position.y
-	_player = get_tree().get_first_node_in_group("player") as Node2D
+	_camera = get_viewport().get_camera_2d()
+
 	warning.hide()
+	obstacle.hide()
+	obstacle.monitoring = false
+	obstacle.body_entered.connect(_on_obstacle_body_entered)
+	obstacle.area_entered.connect(_on_obstacle_area_entered)
 	trigger_box.body_entered.connect(_on_trigger_box_body_entered)
-	missile.hide()
-	missile.monitoring = false
-	missile.body_entered.connect(_on_missile_body_entered)
-	missile.area_entered.connect(_on_missile_area_entered)
 
 
 func _process(delta: float) -> void:
-	if _player == null:
-		_player = get_tree().get_first_node_in_group("player") as Node2D
-		return
+	if _camera == null:
+		_camera = get_viewport().get_camera_2d()
 
 	match _state:
-		"waiting":
+		&"waiting":
 			pass
-		"warning":
+		&"alert":
+			_alert_timer -= delta
 			_update_warning_position()
-			_warning_timer -= delta
-			if _warning_timer <= 0.0:
-				_launch()
-		"launched":
-			_travel_timer += delta
-			global_position.x -= _launch_speed * delta
-			if global_position.x <= impact_point.global_position.x:
-				global_position.x = impact_point.global_position.x
-				if _travel_timer >= travel_time_to_impact:
-					queue_free()
+			if _alert_timer <= 0.0:
+				_launch_obstacle()
+		&"moving":
+			_life_timer -= delta
+			obstacle.global_position.x -= obstacle_speed * delta
+			if _life_timer <= 0.0:
+				queue_free()
 
 
-func _start_warning() -> void:
-	_state = "warning"
-	_warning_timer = warning_time
+func _on_trigger_box_body_entered(body: Node2D) -> void:
+	if _state != &"waiting":
+		return
+	if not (body is PlayerRunner):
+		return
+	_start_alert()
+
+
+func _start_alert() -> void:
+	_state = &"alert"
+	_alert_timer = maxf(alert_duration, 0.0)
 	global_position.y = _lane_y
 	_update_warning_position()
 	warning.show()
 
 
-func _launch() -> void:
+func _launch_obstacle() -> void:
 	var spawn_x := _get_spawn_x()
-	var impact_x := impact_point.global_position.x
-	global_position.x = spawn_x
-	_launch_speed = absf(spawn_x - impact_x) / maxf(travel_time_to_impact, 0.01)
-	_travel_timer = 0.0
+	obstacle.global_position = Vector2(spawn_x, _lane_y)
+	obstacle.show()
+	obstacle.monitoring = true
 	warning.hide()
-	missile.show()
-	missile.monitoring = true
-	_state = "launched"
+	_state = &"moving"
+	_life_timer = maxf(self_destruct_time, 0.0)
 
 
 func _update_warning_position() -> void:
-	var camera := get_viewport().get_camera_2d()
 	var warning_x := global_position.x
-	var warning_y := _lane_y + warning_vertical_offset
+	var warning_y := _lane_y
 
-	if camera:
-		var half_width := get_viewport_rect().size.x * 0.5 / camera.zoom.x
-		warning_x = camera.get_screen_center_position().x + half_width - warning_right_margin
+	if _camera != null:
+		var half_width := get_viewport_rect().size.x * 0.5 / _camera.zoom.x
+		warning_x = _camera.get_screen_center_position().x + half_width - WARNING_RIGHT_MARGIN
 
 	warning.global_position = Vector2(warning_x, warning_y)
 
 
-func _on_trigger_box_body_entered(body: Node2D) -> void:
-	if _state != "waiting":
-		return
-	if body.has_method("take_hit"):
-		_start_warning()
-
-
 func _get_spawn_x() -> float:
-	var camera := get_viewport().get_camera_2d()
-	if camera:
-		return camera.get_screen_center_position().x + (get_viewport_rect().size.x * 0.5 / camera.zoom.x) + spawn_offset_from_camera
-	return _player.global_position.x + trigger_distance
+	if _camera != null:
+		var half_width := get_viewport_rect().size.x * 0.5 / _camera.zoom.x
+		return _camera.get_screen_center_position().x + half_width + SPAWN_OFFSCREEN_MARGIN
+	return global_position.x + SPAWN_OFFSCREEN_MARGIN
 
 
-func _on_missile_body_entered(body: Node2D) -> void:
+func _on_obstacle_body_entered(body: Node2D) -> void:
 	if body.has_method("take_hit"):
 		body.take_hit()
 
 
-func _on_missile_area_entered(area: Area2D) -> void:
-	if can_be_punched and area.name == "PunchArea":
+func _on_obstacle_area_entered(area: Area2D) -> void:
+	if area.name == "PunchArea":
 		queue_free()
